2e8f7d4017a62ca755b5815652230e91ddb91308
EvgeniUPS <evgeniups@gmail.com>
2018-06-27 20:45:27 +0300

    tables test

--- src/components/Tables.vue
+++ src/components/Tables.vue
@@ -43,11 +43,12 @@
 
   </v-container>
   </template>
- 
+
 <script>
 export default {
   data() {
     return {
+      //////
       show: false,
       lorem: `Lorem ipsum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alienum argumentum ius ad. Pri eu justo aeque torquatos.`,
       temp: 1000000,
